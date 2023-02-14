0ba0f7e8f5adfbdb07ed8c07819ba5e39436a3fe
jinser <cmdr.jv@gmail.com>
2023-02-14 18:04:32 +0800

    strip

--- github-extension/lib/github.ts
+++ github-extension/lib/github.ts
@@ -1,5 +1,16 @@
 export async function getAuthedToken(code: string): Promise<string> {
-    let res = await fetch(`https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${code}`);
+    const client_id = process.env.GITHUB_CLIENT_ID;
+    const client_secret = process.env.GITHUB_CLIENT_SECRET;
+
+    const github_base = "https://github.com/login/oauth/access_token"
+    let res = await fetch(
+        `${github_base}?client_id=${client_id}&client_secret=${client_secret}&code=${code}`,
+        {
+            headers: {
+                Accept: "application/json",
+            }
+        }
+    );
     const access = await res.json();
 
     if (!access.access_token) {
