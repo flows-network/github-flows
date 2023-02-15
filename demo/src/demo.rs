fb766b2ac2cd4e770a9bf31d76e721391c0cd049
jinser <cmdr.jv@gmail.com>
2023-02-15 15:37:26 +0800

    demo-ex

--- demo/src/demo.rs
+++ demo/src/demo.rs
@@ -1,13 +1,26 @@
-use github_flows::{listen_to_event, get_octo, Event};
+use github_flows::{
+    get_octo, listen_to_event, octocrab::models::events::payload::EventPayload, Event,
+};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    listen_to_event(handler).await
+    listen_to_event("jetjinser", "github-flows", vec!["issue_comment"], handler).await;
 }
 
 async fn handler(event: Event) {
-        let octo = get_octo();
+    let body = if let Some(EventPayload::IssueCommentEvent(e)) = event.payload {
+        e.comment
+            .body
+            .unwrap_or("oops, there is no comment body".to_string())
+    } else {
+        "oops, it is not a IssueCommentEvent".to_string()
+    };
 
-        octo.issues("jetjinser", "github-flows").create_comment(1, "Hello, World!").await;
+    let octo = get_octo();
+
+    octo.issues("jetjinser", "github-flows")
+        .create_comment(1, format!("Ciao~!\nYou just comment:\n{}", body).as_str())
+        .await
+        .unwrap();
 }
