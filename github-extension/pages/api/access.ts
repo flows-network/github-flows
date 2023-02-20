747d9605cb29e43df85de1bb1eba79dd38798330
jinser <cmdr.jv@gmail.com>
2023-02-20 22:57:26 +0800

    update TODO

--- github-extension/pages/api/access.ts
+++ github-extension/pages/api/access.ts
@@ -1,21 +1,31 @@
+import { redis } from "@/lib/upstash";
 import type { NextApiRequest, NextApiResponse } from "next"
 import httpProxyMiddleware from "next-http-proxy-middleware";
 
 const fn = async (req: NextApiRequest, res: NextApiResponse) => {
     let body = req.body;
 
-    if (body["installation"]) {
+    let installation = body["installation"];
+    if (installation) {
         let action = body["action"];
         if (action == "created") {
-            console.log("saved installation id");
-            console.log(body["installation"]["access_tokens_url"])
+            // TODO: not sure for every kind of installation.
+            let nodeId = installation["account"]["node_id"];
+
+            // TODO: update on already exits.
+            await redis.hset("github:installations", {
+                [nodeId]: [
+                    installation["id"]
+                ],
+            })
+            // TODO: is it work?...
         } else if (action == "deleted") {
-            console.log("deleted installation id");
-            console.log(body["installation"]["access_tokens_url"])
+            // TODO: delete installation token.
         }
     }
 
     return httpProxyMiddleware(req, res, {
+        // TODO:
         // target: "https://code.flows.network/hook/github/message",
         target: "https://httpbin.org/anything",
         pathRewrite: [{
