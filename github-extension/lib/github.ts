8e92b51f94fac4de78a821f30dd5b33f886b5f08
jinser <cmdr.jv@gmail.com>
2023-02-21 15:28:27 +0800

    update gh app in sdk

--- github-extension/lib/github.ts
+++ github-extension/lib/github.ts
@@ -1,6 +1,7 @@
 import { env, exit } from "process";
 
 export const APP_ID = env.GITHUB_APP_ID || exit();
+export const APP_NAME = env.GITHUB_APP_NAME || exit();
 export const CLIENT_ID = env.GITHUB_CLIENT_ID || exit();
 export const CLIENT_SECRET = env.GITHUB_CLIENT_SECRET || exit();
 export const PRIVATE_KEY = env.GITHUB_PRIVATE_KEY || exit();
