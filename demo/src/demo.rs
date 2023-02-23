fe568d9d46a563094a601984843417e5ab1c031e
Jinser <cmdr.jv@gmail.com>
2023-02-23 19:38:04 +0800

    update demo

--- demo/src/demo.rs
+++ demo/src/demo.rs
@@ -7,23 +7,43 @@ use slack_flows::send_message_to_channel;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    listen_to_event("jetjinser", "github-flows", vec!["issue_comment"], handler).await;
+    listen_to_event(
+        "jetjinser",
+        "github-flows",
+        vec!["issue_comment", "issues"],
+        handler,
+    )
+    .await;
 }
 
 async fn handler(payload: EventPayload) {
-    if let EventPayload::IssueCommentEvent(e) = payload {
-        let comment_id = e.comment.id.0;
+    let octo = get_octo(Some(String::from("jetjinser")));
+    let issues = octo.issues("jetjinser", "github-flows");
 
-        let octo = get_octo(Some(String::from("jetjinser")));
+    let reaction = match payload {
+        EventPayload::IssuesEvent(e) => {
+            let issue_id = e.issue.number;
+            Some(
+                issues
+                    .create_reaction(issue_id, ReactionContent::Rocket)
+                    .await,
+            )
+        }
+        EventPayload::IssueCommentEvent(e) => {
+            let comment_id = e.comment.id.0;
+            Some(
+                issues
+                    .create_comment_reaction(comment_id, ReactionContent::Rocket)
+                    .await,
+            )
+        }
+        _ => None
+    };
 
-        let reaction = octo
-            .issues("jetjinser", "github-flows")
-            .create_comment_reaction(comment_id, ReactionContent::Rocket)
-            .await;
-
-        match reaction {
+    if let Some(re) = reaction {
+        match re {
             Ok(c) => send_message_to_channel("ham-5b68442", "general", c.created_at.to_rfc2822()),
             Err(e) => send_message_to_channel("ham-5b68442", "general", e.to_string()),
         }
-    };
+    }
 }
