1872b8d5c83af7bb7fd6c403882b43f13eadd54f
jinser <cmdr.jv@gmail.com>
2023-02-23 13:04:55 +0800

    fix proxy

--- github-integration/pages/api/access.ts
+++ github-integration/pages/api/access.ts
@@ -1,6 +1,24 @@
 import { redis } from "@/lib/upstash";
+import { createProxyMiddleware } from "http-proxy-middleware";
 import type { NextApiRequest, NextApiResponse } from "next"
-import httpProxyMiddleware from "next-http-proxy-middleware";
+
+const restream = function(proxyReq: any, req: any, _res: any, _options: any) {
+    if (req.body) {
+        let bodyData = JSON.stringify(req.body);
+        proxyReq.setHeader('Content-Type', 'application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        proxyReq.write(bodyData);
+    }
+}
+
+const proxyMiddleware: any = createProxyMiddleware({
+    target: "https://code.flows.network/hook/github/message",
+    changeOrigin: true,
+    pathRewrite: {
+        "^/api/access": "",
+    },
+    onProxyReq: restream,
+});
 
 const fn = async (req: NextApiRequest, res: NextApiResponse) => {
     let body = req.body;
@@ -21,13 +39,17 @@ const fn = async (req: NextApiRequest, res: NextApiResponse) => {
         }
     }
 
-    return httpProxyMiddleware(req, res, {
-        target: "https://code.flows.network/hook/github/message",
-        pathRewrite: [{
-            patternStr: `^/api/access`,
-            replaceStr: "",
-        }]
-    })
+    proxyMiddleware(req, res, (result: unknown) => {
+        if (result instanceof Error) {
+            throw result;
+        }
+    });
 };
 
 export default fn;
+
+export const config = {
+    api: {
+        externalResolver: true,
+    },
+};
