5dd395db0045a1f0ffc91afc48823831c0bfb442
jinser <cmdr.jv@gmail.com>
2023-03-17 20:38:09 +0800

    Update: extra isItMe

--- github-integration/lib/github.ts
+++ github-integration/lib/github.ts
@@ -4,3 +4,26 @@ export const APP_ID = env.GITHUB_APP_ID || exit();
 export const APP_NAME = env.GITHUB_APP_NAME || exit();
 export const CLIENT_ID = env.GITHUB_CLIENT_ID || exit();
 export const CLIENT_SECRET = env.GITHUB_CLIENT_SECRET || exit();
+
+export async function isItMe(login: string, token: string): Promise<Boolean> {
+    let url = "https://api.github.com/user";
+    let resp = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Accept": "application/vnd.github.v3+json",
+            "User-Agent": "GitHub Integration of Second State flows.network",
+            "Authorization": `Bearer ${token}`
+        },
+    });
+
+
+    try {
+        if (resp.ok) {
+            let json = await resp.json();
+            return json["login"] == login;
+        }
+        return false;
+    } catch {
+        return false;
+    }
+}
--- github-integration/pages/api/access.ts
+++ github-integration/pages/api/access.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { CLIENT_ID, CLIENT_SECRET } from "lib/github";
+import { CLIENT_ID, CLIENT_SECRET, isItMe } from "lib/github";
 import { decrypt, State } from "@/lib/state";
 import { redis } from "@/lib/upstash";
 
@@ -56,29 +56,6 @@ const fn = async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     return res.redirect("https://flows.network/flows");
-}
-
-async function isItMe(login: string, token: string): Promise<Boolean> {
-    let url = "https://api.github.com/user";
-    let resp = await fetch(url, {
-        method: "GET",
-        headers: {
-            "Accept": "application/vnd.github.v3+json",
-            "User-Agent": "GitHub Integration of Second State flows.network",
-            "Authorization": `Bearer ${token}`
-        },
-    });
-
-
-    try {
-        if (resp.ok) {
-            let json = await resp.json();
-            return json["login"] == login;
-        }
-        return false;
-    } catch {
-        return false;
-    }
 }
 
 export default fn;
