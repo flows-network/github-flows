ec108319fb840cd9b91d6c5f43e14f44631bfebc
jinser <cmdr.jv@gmail.com>
2023-03-17 20:36:54 +0800

    Remove: pk

--- github-integration/lib/github.ts
+++ github-integration/lib/github.ts
@@ -4,4 +4,3 @@ export const APP_ID = env.GITHUB_APP_ID || exit();
 export const APP_NAME = env.GITHUB_APP_NAME || exit();
 export const CLIENT_ID = env.GITHUB_CLIENT_ID || exit();
 export const CLIENT_SECRET = env.GITHUB_CLIENT_SECRET || exit();
-export const PRIVATE_KEY = env.GITHUB_PRIVATE_KEY || exit();
